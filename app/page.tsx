d6703d77dbbd06d15cbe51ca8f0732c3e74acd84
Simon Manzler <simanzler@gmail.com>
2025-12-01 16:18:30 -0500

    removed index metatags

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
-import type { Metadata } from "next";
 import { Generator } from "../components/generator";
 
 export type { Material } from "../components/generator";
-
-export const metadata: Metadata = {
-  title: "Generator",
-  description:
-    "Generate a visual material list for your Minecraft builds. Input your materials and see them displayed with images, quantities, and stack counts.",
-  openGraph: {
-    title: "Minecraft Material List Generator - Generator",
-    description:
-      "Generate a visual material list for your Minecraft builds. Input your materials and see them displayed with images, quantities, and stack counts.",
-  },
-  twitter: {
-    title: "Minecraft Material List Generator - Generator",
-    description:
-      "Generate a visual material list for your Minecraft builds. Input your materials and see them displayed with images, quantities, and stack counts.",
-  },
-};
 
 export default function Home() {
   return (
